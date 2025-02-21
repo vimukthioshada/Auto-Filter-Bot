d30add1a211df6f6e357b2aa317798ee8ba55d60
vimukthioshada <77786139+vimukthioshada@users.noreply.github.com>
2025-02-21 06:32:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://t.me/FilmStudiohub2")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
